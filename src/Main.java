ad801cd5bebe989df146bfe4e00fc7d39115cb10
A-nDr32-O <ac7143589@gmail.com>
2024-09-30 10:46:25 -0500

    Suma de 2 enteros

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world!");
+
+        int we=10;
+        int wq=23;
+
+        int suma = we+wq;
+
+        System.out.println("La suma es:"+ suma);
+    }
+}
